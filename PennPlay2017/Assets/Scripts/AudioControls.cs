1ad64de9bcd0ca795c1fcd4d365fb3ae461b2760
hydroguy45 <foleychriscpfjr@gmail.com>
2017-10-14 14:05:40 -0400

    fuck FFTs... also Hi twitter

--- PennPlay2017/Assets/Scripts/AudioControls.cs
+++ PennPlay2017/Assets/Scripts/AudioControls.cs
@@ -19,7 +19,7 @@ public class AudioControls : MonoBehaviour {
 		}*/
 		source.clip = Microphone.Start (mic, true, timeLength, maxFreq); 
 		//GetComponent<AudioSource>().loop = true;
-		//audio.mute = true;
+		source.mute = true;
 		while(!(Microphone.GetPosition(mic) > 0)){}//Busy wait
 		source.Play();
 	}
@@ -29,8 +29,7 @@ public class AudioControls : MonoBehaviour {
 		timeElapsedSinceUpdate += Time.deltaTime;
 		if (timeElapsedSinceUpdate >= refreshTime) {
 			StopMic ();
-			Debug.Log ("Processing Audio Segment");
-			//TODO: process the Recorded Data
+			ProcessMicData();
 			StartMic ();
 			timeElapsedSinceUpdate = 0f;
 		}
@@ -50,5 +49,24 @@ public class AudioControls : MonoBehaviour {
 		source.clip = Microphone.Start (mic, true, timeLength, maxFreq);
 		while(!(Microphone.GetPosition(mic) > 0)){}//Busy wait
 		source.Play(); 
+	}
+
+	void ProcessMicData(){
+		int numberOfBins = 1024;
+		float[] spectrum = new float[numberOfBins];
+		AudioListener.GetSpectrumData(spectrum, 1, FFTWindow.Hamming);
+		int maxFreq = 1; //Nyquist freq is a thing
+		for (int i = 1; i < spectrum.Length - 1; i++)
+		{
+			if (spectrum [i] > spectrum [maxFreq]) {
+				maxFreq = i;
+			}
+			Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red, Time.deltaTime, false);
+			Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+			Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+			//Cyan seems to be what to use
+			Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+		}
+		Debug.Log (maxFreq);
 	}
 }
